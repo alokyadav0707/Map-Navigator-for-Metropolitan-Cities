de4fbdbfcb24342720c28fa11ac689f9b9e667d8
alokyadav0707 <mralokyadav07@gmail.com>
2025-09-05 05:08:26 +0530

    Update Metroapp.cpp

--- Metroapp.cpp
+++ Metroapp.cpp
@@ -1,106 +1,95 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <unordered_map>
-#include <climits>
+#include <bits/stdc++.h>
 using namespace std;
 
-// Structure to represent a node in the priority queue
-struct Node {
-    int vertex;
-    int cost;
-    int time;
+struct Edge {
+    int to;
+    double time;   // effective travel time (mins)
+    double dist;   // physical distance (km)
+};
 
-    bool operator<(const Node& other) const {
-        return cost > other.cost; // Min-heap based on cost
+struct State {
+    int node;
+    double time, dist;
+    bool operator>(const State &other) const {
+        if (fabs(time - other.time) < 1e-9) return dist > other.dist;  // distance as the second priority if time is same
+        return time > other.time;    // time is the first priority
     }
 };
 
-class MapNavigator {
-private:
-    int numVertices;
-    vector<vector<pair<int, pair<int, int>>>> adjList; // {neighbor, {distance, time}}
+vector<Edge> graph[100005];
 
-public:
-    MapNavigator(int vertices) : numVertices(vertices) {
-        adjList.resize(vertices);
-    }
+// ---------- Constraint Models ----------
+double roadTime(double distance, double avgSpeed, int numSignals, double signalDelay) {
+    double travel = (distance / avgSpeed) * 60.0;  // hr → min
+    return travel + numSignals * signalDelay;
+}
 
-    void addEdge(int u, int v, int distance, int time) {
-        adjList[u].emplace_back(v, make_pair(distance, time));
-        adjList[v].emplace_back(u, make_pair(distance, time)); // For undirected graph
-    }
+double metroTime(double distance, double metroSpeed, double avgWait) {
+    double travel = (distance / metroSpeed) * 60.0;
+    return travel + avgWait;
+}
 
-    void findOptimalRoute(int start, int end) {
-        priority_queue<Node> pq;
-        vector<int> distance(numVertices, INT_MAX);
-        vector<int> travelTime(numVertices, INT_MAX);
-        vector<int> parent(numVertices, -1);
+// ---------- Dijkstra ----------
+pair<double, double> dijkstra(int src, int dest, int n) {
+    vector<double> minTime(n, 1e18);
+    vector<double> minDist(n, 1e18);
 
-        distance[start] = 0;
-        travelTime[start] = 0;
-        pq.push({start, 0, 0});
+    priority_queue<State, vector<State>, greater<State>> pq;
+    pq.push({src, 0, 0});
+    minTime[src] = 0;
+    minDist[src] = 0;
 
-        while (!pq.empty()) {
-            Node current = pq.top();
-            pq.pop();
+    while (!pq.empty()) {
+        auto cur = pq.top(); pq.pop();
+        int u = cur.node;
 
-            int u = current.vertex;
-            for (const auto& neighbor : adjList[u]) {
-                int v = neighbor.first;
-                int weight = neighbor.second.first;
-                int time = neighbor.second.second;
+        if (u == dest) return {cur.time, cur.dist};
 
-                if (distance[u] + weight < distance[v] || 
-                    (distance[u] + weight == distance[v] && travelTime[u] + time < travelTime[v])) {
+        for (auto &e : graph[u]) {
+            double newTime = cur.time + e.time;
+            double newDist = cur.dist + e.dist;
 
-                    distance[v] = distance[u] + weight;
-                    travelTime[v] = travelTime[u] + time;
-                    parent[v] = u;
-                    pq.push({v, distance[v], travelTime[v]});
-                }
+            if (newTime < minTime[e.to] ||
+               (fabs(newTime - minTime[e.to]) < 1e-9 && newDist < minDist[e.to])) {
+                minTime[e.to] = newTime;
+                minDist[e.to] = newDist;
+                pq.push({e.to, newTime, newDist});
             }
         }
-
-        if (distance[end] == INT_MAX) {
-            cout << "No route exists from " << start << " to " << end << ".\n";
-        } else {
-            cout << "Optimal Route from " << start << " to " << end << ":\n";
-            printPath(parent, end);
-            cout << "\nTotal Distance: " << distance[end] << " units\n";
-            cout << "Total Time: " << travelTime[end] << " minutes\n";
-        }
     }
+    return {-1, -1}; // no path found
+}
 
-    void printPath(const vector<int>& parent, int vertex) {
-        if (parent[vertex] == -1) {
-            cout << vertex;
-            return;
-        }
-        printPath(parent, parent[vertex]);
-        cout << " -> " << vertex;
-    }
-};
-
+// ---------- Main ----------
 int main() {
-    int vertices, edges;
-    cout << "Enter number of intersections (vertices) and roads (edges): ";
-    cin >> vertices >> edges;
+    int n = 5; // number of nodes
+    int src = 0, dest = 4;
 
-    MapNavigator navigator(vertices);
+    // Road edge: 5 km, 30 km/h, 2 signals with 1.5 min avg delay
+    double t1 = roadTime(5.0, 30.0, 2, 1.5);
+    graph[0].push_back({1, t1, 5.0});
 
-    cout << "Enter the roads as (start, end, distance, time):\n";
-    for (int i = 0; i < edges; ++i) {
-        int u, v, distance, time;
-        cin >> u >> v >> distance >> time;
-        navigator.addEdge(u, v, distance, time);
-    }
+    // Road edge: 12 km, 40 km/h, 1 signal of 2 mins
+    double t2 = roadTime(12.0, 40.0, 1, 2.0);
+    graph[1].push_back({2, t2, 12.0});
 
-    int start, end;
-    cout << "Enter starting and ending intersections: ";
-    cin >> start >> end;
+    // Metro edge: 4 km, 40 km/h, avg wait 3 mins
+    double t3 = metroTime(4.0, 40.0, 3.0);
+    graph[0].push_back({3, t3, 4.0});
 
-    navigator.findOptimalRoute(start, end);
+    // Metro edge: 10 km, 50 km/h, avg wait 4 mins
+    double t4 = metroTime(10.0, 50.0, 4.0);
+    graph[3].push_back({4, t4, 10.0});
+
+    // Road edge: 2 km, 20 km/h, 0 signals
+    double t5 = roadTime(2.0, 20.0, 0, 0.0);
+    graph[2].push_back({4, t5, 2.0});
+
+    auto ans = dijkstra(src, dest, n);
+
+    if (ans.first < 0) cout << "No path found\n";
+    else cout << "Minimum Time = " << ans.first 
+              << " mins, Distance = " << ans.second << " km\n";
 
     return 0;
 }
